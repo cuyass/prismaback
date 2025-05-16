91e4ab4f3c0fbc4d05458372c39c0f6d921ea66b
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-16 12:12:30 +0200

    first commit, setting up project's structure

---
+++ src/main/java/com/prismaback/prismaback/PrismabackApplication.java
@@ -0,0 +1,21 @@
+package com.prismaback.prismaback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.*;
+
+@SpringBootApplication
+public class PrismabackApplication {
+
+	public static void main(String[] args) {
+
+		Dotenv dotenv = Dotenv.load();
+		System.setProperty("DATABASE_URL", dotenv.get("DATABASE_URL"));
+		System.setProperty("DATABASE_USERNAME", dotenv.get("DATABASE_USERNAME"));
+		System.setProperty("DATABASE_PASSWORD", dotenv.get("DATABASE_PASSWORD"));
+
+		SpringApplication.run(PrismabackApplication.class, args);
+		System.out.println("Startup sequence complete: Category is... Java Realness! 10s, 10s, 10s across the board!");
+	}
+
+}
---
+++ src/test/java/com/prismaback/prismaback/PrismabackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.prismaback.prismaback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PrismabackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
