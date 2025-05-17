70b76fe000e3c2b9880b35c6616adf358c70e3d7
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-17 10:14:30 +0200

    feat(dev): add CORS bean to lesson controller, ready to roll with the front

--- src/main/java/com/prismaback/prismaback/controller/LessonController.java
+++ src/main/java/com/prismaback/prismaback/controller/LessonController.java
@@ -13,6 +13,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/api/v1/lessons")
 @RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
 public class LessonController {
 
     private final LessonService lessonService;
