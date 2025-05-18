60c5239c6f91d174acaf9ecdb88b923b2226c102
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-18 19:56:08 +0200

    feat(question): create question repo

---
+++ src/main/java/com/prismaback/prismaback/repository/QuestionRepository.java
@@ -0,0 +1,14 @@
+package com.prismaback.prismaback.repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.prismaback.prismaback.model.Question;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    List<Question> findByQuizId(Long quizId);
+}
+
