edabca3d0c1f9008dd0b56139d2de75784907d40
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-18 20:33:18 +0200

    fix(quiz): fix quiz controller to fit DTO

--- src/main/java/com/prismaback/prismaback/controller/QuizController.java
+++ src/main/java/com/prismaback/prismaback/controller/QuizController.java
@@ -1,39 +1,34 @@
 package com.prismaback.prismaback.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import com.prismaback.prismaback.model.Quiz;
-import com.prismaback.prismaback.service.QuizService;
-
-import lombok.RequiredArgsConstructor;
-
-import java.util.List;
-
-import com.prismaback.prismaback.model.Question;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prismaback.prismaback.DTO.QuizDTO;
+import com.prismaback.prismaback.service.QuizService;
+
+import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/api/quizzes")
+@RequestMapping("/api/v1/quizzes")
 @RequiredArgsConstructor
 public class QuizController {
     private final QuizService quizService;
 
     @PostMapping("/{lessonId}")
-    public ResponseEntity<Quiz> createQuiz(
-            @PathVariable Long lessonId,
-            @RequestBody List<Question> questions) {
-        Quiz quiz = quizService.createQuiz(lessonId, questions);
-        return ResponseEntity.ok(quiz);
+    public ResponseEntity<QuizDTO> createQuiz(@RequestBody QuizDTO quizDTO)
+             {
+                QuizDTO createdQuiz = quizService.createQuiz(quizDTO);
+                return ResponseEntity.ok(createdQuiz);
     }
 
     @GetMapping("/lesson/{lessonId}")
-    public ResponseEntity<Quiz> getQuizByLesson(@PathVariable Long lessonId) {
+    public ResponseEntity<QuizDTO> getQuizByLesson(@PathVariable Long lessonId) {
         return quizService.getQuizByLessonId(lessonId)
                 .map(ResponseEntity::ok)
                 .orElse(ResponseEntity.notFound().build());
