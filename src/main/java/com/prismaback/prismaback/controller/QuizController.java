4523710743150a5b731064797b9ca41f8b48ab71
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-18 19:48:05 +0200

    feat(quiz): create quiz controller

---
+++ src/main/java/com/prismaback/prismaback/controller/QuizController.java
@@ -0,0 +1,41 @@
+package com.prismaback.prismaback.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.prismaback.prismaback.model.Quiz;
+import com.prismaback.prismaback.service.QuizService;
+
+import lombok.RequiredArgsConstructor;
+
+import java.util.List;
+
+import org.aspectj.weaver.patterns.TypePatternQuestions.Question;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+
+@RestController
+@RequestMapping("/api/quizzes")
+@RequiredArgsConstructor
+public class QuizController {
+    private final QuizService quizService;
+
+    @PostMapping("/{lessonId}")
+    public ResponseEntity<Quiz> createQuiz(
+            @PathVariable Long lessonId,
+            @RequestBody List<Question> questions) {
+        Quiz quiz = quizService.createQuiz(lessonId, questions);
+        return ResponseEntity.ok(quiz);
+    }
+
+    @GetMapping("/lesson/{lessonId}")
+    public ResponseEntity<Quiz> getQuizByLesson(@PathVariable Long lessonId) {
+        return quizService.getQuizByLessonId(lessonId)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
+    }
+}
