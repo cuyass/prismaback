058edc8726b21558ff7042809ede851dda5958bd
Mariona Cuyàs <cuyas.mariona@gmail.com>
2025-05-21 14:49:38 +0200

    feat(user): create user controller

---
+++ src/main/java/com/prismaback/prismaback/controller/UserController.java
@@ -0,0 +1,33 @@
+package com.prismaback.prismaback.controller;
+
+import com.prismaback.prismaback.DTO.UserDTO;
+import com.prismaback.prismaback.service.UserService;
+
+import lombok.RequiredArgsConstructor;
+
+import com.prismaback.prismaback.model.User;
+import com.prismaback.prismaback.repository.UserRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/users")
+@RequiredArgsConstructor
+@CrossOrigin(origins = "http://localhost:5173")
+public class UserController {
+    
+    private final UserRepository userRepository;
+
+    @PostMapping
+    public ResponseEntity<UserDTO> registerUser(@RequestBody UserDTO userDto) {
+        if (userDto.getEmail() == null || userDto.getEmail().isEmpty()) {
+            return ResponseEntity.badRequest().build();
+        }
+        if (userRepository.existsByEmail(userDto.getEmail())) {
+            return ResponseEntity.badRequest().build();
+        }
+        User user = UserService.toEntity(userDto);
+        User newUser = userRepository.save(user);
+        return ResponseEntity.ok(UserService.toDto(newUser));
+    }
+}
--- src/main/java/com/prismaback/prismaback/repository/UserRepository.java
+++ src/main/java/com/prismaback/prismaback/repository/UserRepository.java
@@ -4,6 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.prismaback.prismaback.model.User;
 
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findByEmail(String email);
     boolean existsByEmail(String email);
 }
